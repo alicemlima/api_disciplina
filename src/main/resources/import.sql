32e1d519235670a757abc4c6614ac3b419491090
herlmanoel <herlmanoelfernandes@gmail.com>
2022-01-27 10:45:13 -0300

    feat(sql): add script de inserção de dados

---
+++ src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO `disciplina`(`nome`, `descricao`) VALUES ('Matemática', 'Descrição 01');
+INSERT INTO `disciplina`(`nome`, `descricao`) VALUES ('História', 'Descrição 01');
+INSERT INTO `disciplina`(`nome`, `descricao`) VALUES ('Geografia', 'Descrição 01');
+INSERT INTO `disciplina`(`nome`, `descricao`) VALUES ('Física', 'Descrição 01');
+INSERT INTO `disciplina`(`nome`, `descricao`) VALUES ('Biologia', 'Descrição 01');
+INSERT INTO `disciplina`(`nome`, `descricao`) VALUES ('Português', 'Descrição 01');
+
+INSERT INTO `aluno` (`nome`, `email`, `telefone`, `cpf`) VALUES  ('João Carlos', 'joao@gmail.com', '84991800002', '216.747.480-60');
+INSERT INTO `aluno` (`nome`, `email`, `telefone`, `cpf`) VALUES  ('Leonardo Silva', 'leo@gmail.com', '84991800002', '481.473.250-37');
+INSERT INTO `aluno` (`nome`, `email`, `telefone`, `cpf`) VALUES  ('Maria Silva', 'maria@gmail.com', '84991800002', '121.026.180-48');
+
+INSERT INTO `professor` (`nome`, `email`, `telefone`, `cpf`) VALUES  ('Ruan Gabriel', 'ruan@gmail.com', '84991800002', '365.910.750-62');
+INSERT INTO `professor` (`nome`, `email`, `telefone`, `cpf`) VALUES  ('Júlia Santos', 'julia@gmail.com', '84991800002', '666.301.300-90');
+INSERT INTO `professor` (`nome`, `email`, `telefone`, `cpf`) VALUES  ('Eduarda Costa', 'edu@gmail.com', '84991800002', '259.272.150-92');
