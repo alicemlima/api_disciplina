a9ef5dad3763985feeb8c3db17071140f306e7a1
herlmanoel <herlmanoelfernandes@gmail.com>
2022-01-30 16:58:44 -0300

    feat(repository): add assinatura para buscar disciplina pelo nome

--- src/main/java/com/api_curso/domain/repositories/DisciplinaRepository.java
+++ src/main/java/com/api_curso/domain/repositories/DisciplinaRepository.java
@@ -3,6 +3,11 @@ package com.api_curso.domain.repositories;
 import com.api_curso.domain.entities.Disciplina;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
+
+    Optional<Disciplina> findByNome(String nome);
+
 }
